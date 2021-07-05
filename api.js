95ece9d960e3b26a11b989466fbb60a7c91315d2
YORGO TABET <yorgotabet@gmail.com>
2021-07-05 21:11:52 +0300

    commit

---
+++ api.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const xlsx = require('xlsx');
+const cors = require('cors')
+app.use(cors())
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+ 
+app.get('/getServices',cors(corsOptions), function (req, res) {
+  
+    var wb = xlsx.readFile('database.xlsx');
+    var ws = wb.Sheets[wb.SheetNames[0]]; 
+    let data = xlsx.utils.sheet_to_json(ws) 
+    res.send(data)
+})
+ 
+app.listen(3000)
